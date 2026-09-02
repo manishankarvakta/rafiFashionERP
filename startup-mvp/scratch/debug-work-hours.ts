66c7fc464da6c79adf285a7d3e7dad174803cddb
dev-mhratul <dev.mhratul@gmail.com>
2026-09-02 18:12:31 +0600

    update

--- startup-mvp/scratch/debug-work-hours.ts
+++ startup-mvp/scratch/debug-work-hours.ts
@@ -2,7 +2,7 @@ import { prisma } from "../lib/prisma";
 import { getShiftWindow, calculateWorkHoursWithBreak, calculateOTHours } from "../lib/hr/shift-utils";
 import { differenceInMinutes } from "date-fns";
 
-// 
+
 
 async function debugWorkHours() {
   const dateStr = "2026-07-28";
